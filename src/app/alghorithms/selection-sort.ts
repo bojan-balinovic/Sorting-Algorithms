cc1f02323eedd1993d64d64a0612a2b65e8be66a
Bojan Balinovic <bojanbalinovi@gmail.com>
2023-05-05 15:31:07 +0200

    feat: selection sort

---
+++ src/app/alghorithms/selection-sort.ts
@@ -0,0 +1,25 @@
+import { SortStrategy } from './sort-strategy';
+
+export class SelectionSort extends SortStrategy {
+  override sort(
+    nodes: any[],
+    nodeSwapCallback: (currentNodesState: any[]) => void
+  ): Promise<any[]> {
+    return new Promise(async (resolve) => {
+      // here starts the algorithm
+      for (let i = 0; i < nodes.length - 1; i++) {
+        let min = i;
+        for (let j = i + 1; j < nodes.length; j++) {
+          if (nodes[min].value > nodes[j].value) {
+            min = j;
+          }
+        }
+        let temp = nodes[i];
+        nodes[i] = nodes[min];
+        nodes[min] = temp;
+        await nodeSwapCallback(nodes);
+      }
+      resolve(nodes);
+    });
+  }
+}
